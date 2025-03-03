0a8ceca095398cbcc97d9ed201d76e8258a6eb88
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2025-03-04 02:25:28 +0800

    fix address UI into 1 address

--- accounts/forms.py
+++ accounts/forms.py
@@ -21,6 +21,7 @@ class UserForm(forms.ModelForm):
             )
             
 class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter restaurant location', 'required':'required'}))
     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator]) # optional adjust css file input
     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     
@@ -30,7 +31,7 @@ class UserProfileForm(forms.ModelForm):
     
     class Meta:
         model = UserProfile
-        fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2', 'country', 'state', 'city', 'pin_code', 'latitude', 'longtitude']
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longtitude']
 
     # Way2 : readable only form (complex and handy)
     def __init__(self, *args, **kwargs):
