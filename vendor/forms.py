36547d87fb23989a550dbd9af10114995447aefa
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2025-03-04 01:31:54 +0800

    create edit form UI

--- vendor/forms.py
+++ vendor/forms.py
@@ -5,9 +5,6 @@ from vendor.models import Vendor
 
 
 class VendorForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    confirm_password = forms.CharField(widget=forms.PasswordInput())
-    
     vendor_license = forms.ImageField(widget=forms.FileInput(attrs={'class': 'btn btn-info'})) # optional adjust css file input
     
     class Meta:
--- vendor/views.py
+++ vendor/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
 
 from accounts.forms import UserProfileForm
 from accounts.models import UserProfile
@@ -9,8 +10,21 @@ def restaurant_profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     vendor = get_object_or_404(Vendor, user=request.user)
     
-    profile_form = UserProfileForm(instance=profile)
-    vendor_form = VendorForm(instance=vendor)
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        vendor_form = VendorForm(request.POST, request.FILES, instance=vendor)
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request, 'Setting updated.')
+            return redirect('restaurant_profile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    else:
+        profile_form = UserProfileForm(instance=profile)
+        vendor_form = VendorForm(instance=vendor)
+        
     context = {
         'profile_form': profile_form,
         'vendor_form': vendor_form,
