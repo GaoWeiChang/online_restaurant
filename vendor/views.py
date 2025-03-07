4e1e66a1308f68c6f82e0aef478f47931379cf7b
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2025-03-07 14:33:53 +0800

    create edit category API

--- vendor/views.py
+++ vendor/views.py
@@ -49,7 +49,7 @@ def restaurant_profile(request):
 @user_passes_test(check_role_restaurant)
 def menu_builder(request):
     vendor = get_vendor(request)
-    categories = Category.objects.filter(vendor=vendor)
+    categories = Category.objects.filter(vendor=vendor).order_by('created_at') # order by attribute "created_at"
     context = {
         'categories': categories
     }
@@ -88,3 +88,25 @@ def add_category(request):
         'form': form,
     }
     return render(request, 'restaurant/add_category.html', context)
+
+def edit_category(request, pk=None):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            category_name = form.cleaned_data['category_name']
+            category = form.save(commit=False)
+            category.vendor = get_vendor(request) # vendor
+            category.slug = slugify(category_name) # slug
+            form.save()
+            messages.success(request, 'Category updated successfully!')
+            return redirect('menu_builder')
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm(instance=category)
+    context = {
+        'form': form,
+        'category': category,
+    }
+    return render(request, 'restaurant/edit_category.html', context)
