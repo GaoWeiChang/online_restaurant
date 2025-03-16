420fc4744264b0e57f6e998087dc9e1b84200b85
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2025-03-16 20:23:07 +0800

    add get nearby restaurant in search functionality

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from marketplace.context_processors import get_cart_amounts, get_cart_counter
 from marketplace.models import Cart
@@ -8,6 +8,9 @@ from vendor.models import Vendor
 from django.db.models import Prefetch
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D  # ``D`` is a shortcut for ``Distance``
+from django.contrib.gis.db.models.functions import Distance
 
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8] # [:8] = get 8 restaurants
@@ -111,24 +114,35 @@ def delete_cart(request, cart_id):
         return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
     
 def search(request):
-    address = request.GET['address'] # name in the input tag (name="address")
-    latitude = request.GET['lat']
-    longtitude = request.GET['lng']
-    radius = request.GET['radius']
-    keyword = request.GET['keyword']
-    
+    if 'address' not in request.GET:
+        return redirect('marketplace')
+    else:
+        address = request.GET['address'] # name in the input tag (name="address")
+        latitude = request.GET['lat']
+        longtitude = request.GET['lng']
+        radius = request.GET['radius']
+        keyword = request.GET['keyword']
     
     # get food name
     fetch_vendor_by_fooditem = FoodItem.objects.filter(food_title__icontains=keyword, is_available=True).values_list('vendor', flat=True) # ดึงข้อมูลเฉพาะคอลัมน์ 'vendor' ออกมาเป็นลิสต์แบบเรียบ (flat list)
     
     # ใช้ Q เพื่อสร้างเงื่อนไขการค้นหาที่ซับซ้อนและยืดหยุ่น โดยเฉพาะเมื่อต้องการใช้ operator ทางตรรกศาสตร์ (logical operators) เช่น OR (|) หรือ AND (&)
     vendors = Vendor.objects.filter(Q(id__in=fetch_vendor_by_fooditem) | Q(vendor_name__icontains=keyword, is_approved=True, user__is_active=True))
-    
+    if latitude and longtitude and radius:
+        # Distances will be calculated from this point, which does not have to be projected.
+        pnt = GEOSGeometry('POINT(%s %s)' %(longtitude, latitude))
+        vendors = Vendor.objects.filter(Q(id__in=fetch_vendor_by_fooditem) | Q(vendor_name__icontains=keyword, is_approved=True, user__is_active=True), 
+                                        user_profile__location__distance_lte=(pnt, D(km=radius))
+                                        ).annotate(distance=Distance('user_profile__location', pnt)).order_by('distance')
+        for v in vendors:
+            v.kms = round(v.distance.km, 1)
+        
     vendor_count = vendors.count()
     
     context = {
         'vendors': vendors,
         'vendor_count': vendor_count,
+        'source_location': address,
     }
     
     return render(request, 'marketplace/listings.html', context)
