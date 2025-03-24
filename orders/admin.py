f31e16d37cdf96bdf8e35d17dc3598885ae14a3f
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2025-03-24 23:12:43 +0800

    create order, payment, orderedfood models

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Payment, Order, OrderedFood
 
-# Register your models here.
+# register the model to allow it to be viewed in the admin panel
+admin.site.register(Payment) 
+admin.site.register(Order)
+admin.site.register(OrderedFood)
