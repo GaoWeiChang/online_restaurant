96a569fb29a2b2ad296f1fb285f5aee0ad72a481
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2025-03-26 22:40:09 +0800

    set up paypal payment connection

--- main_app/settings.py
+++ main_app/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'marketplace.context_processors.get_cart_counter',
                 'marketplace.context_processors.get_cart_amounts',
                 'accounts.context_processors.get_user_profile',
+                'accounts.context_processors.get_paypal_client_id',
             ],
         },
     },
@@ -173,3 +174,9 @@ GOOGLE_API_KEY = config('GOOGLE_API_KEY')
 os.environ['PATH'] = os.path.join(BASE_DIR, 'env\\Lib\\site-packages\\osgeo') + ';' + os.environ['PATH']
 os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'env\\Lib\\site-packages\\osgeo\\data\\proj') + ';' + os.environ['PATH']
 GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'env\\Lib\\site-packages\\osgeo\\gdal.dll')
+
+# Paypal client id
+PAYPAL_CLIENT_ID = config('PAYPAL_CLIENT_ID')
+
+# set CORS 
+SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
